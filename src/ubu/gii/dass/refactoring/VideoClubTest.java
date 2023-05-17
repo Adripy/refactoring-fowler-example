a607d948fedf9fb0f2f07da473b1e3b9e35d85c7
Adripy <adri_cordero89@hotmail.com>
2023-05-18 00:16:36 +0200

    Añadidos test
    Se añaden los test necesarios para comprobar el código y obtener la cobertura necesaria

--- src/ubu/gii/dass/refactoring/VideoClubTest.java
+++ src/ubu/gii/dass/refactoring/VideoClubTest.java
@@ -20,6 +20,8 @@ import org.junit.Test;
 public class VideoClubTest {
 	protected Movie m0, m11, m12, m2;
 	protected Customer c1;
+	private static final String ANY_TITLE = "Any Movie Title";
+	private static final int UNKNOWN_PRICE_CODE = 99;
 	
 	@Before
 	public void setUp() {
@@ -56,4 +58,81 @@ public class VideoClubTest {
 
 	}
 
+	@Test
+	public void testMovieRegular() {
+		Movie aMovie = new Movie(ANY_TITLE,Movie.REGULAR);
+		assertEquals(Movie.REGULAR, aMovie.getPriceCode());
+	}
+
+	@Test
+	public void testMovieChildren() {
+		Movie aMovie = new Movie(ANY_TITLE,Movie.CHILDRENS);
+		assertEquals(Movie.CHILDRENS, aMovie.getPriceCode());
+	}
+
+	@Test
+	public void testMovieNewRelease() {
+		Movie aMovie = new Movie(ANY_TITLE,Movie.NEW_RELEASE);
+		assertEquals(Movie.NEW_RELEASE, aMovie.getPriceCode());
+	}
+
+	@Test
+	public void testFrequentRenterPointsRegular() {
+		int points[] = { 1, 1, 1 };
+		Movie aMovie = new Movie(ANY_TITLE,Movie.REGULAR);
+		for (int daysRented = 1; daysRented <= points.length; daysRented++) {
+			assertEquals(points[daysRented-1], aMovie.getFrequentRenterPoints(daysRented));
+		}
+	}
+
+	@Test
+	public void testFrequentRenterPointsChildrens() {
+		int points[] = { 1, 1, 1 };
+		Movie aMovie = new Movie(ANY_TITLE,Movie.CHILDRENS);
+		for (int daysRented = 1; daysRented <= points.length; daysRented++) {
+			assertEquals(points[daysRented-1], aMovie.getFrequentRenterPoints(daysRented));
+		}
+	}
+
+	@Test
+	public void testFrequentRenterPointsNewRelease() {
+	int points[] = { 1, 2, 2, 2 };
+		Movie aMovie = new Movie(ANY_TITLE,Movie.NEW_RELEASE);
+		for (int daysRented = 1; daysRented <= points.length; daysRented++) {
+			assertEquals(points[daysRented-1], aMovie.getFrequentRenterPoints(daysRented));
+		}
+	}
+
+	@Test
+	public void testChargeForRegular() {
+	double charges[] = { 2.0, 2.0, 3.5, 5.0, 6.5, 8.0 };
+		Movie aMovie = new Movie(ANY_TITLE,Movie.REGULAR);
+		for (int daysRented = 1; daysRented <= charges.length; daysRented++) {
+			assertEquals(charges[daysRented-1], aMovie.getCharge(daysRented),0);
+		}
+	}
+
+	@Test
+	public void testChargeForChildrens() {
+		double charges[] = { 1.5, 1.5, 1.5, 3.0, 4.5, 6.0, 7.5 };
+		Movie aMovie = new Movie(ANY_TITLE,Movie.CHILDRENS);
+		for (int daysRented = 1; daysRented <= charges.length; daysRented++) {
+			assertEquals(charges[daysRented-1], aMovie.getCharge(daysRented),0);
+		}
+	}
+
+	@Test
+	public void testChargeNewRelease() {
+		double charges[] = { 3.0, 6.0, 9.0, 12.0 };
+		Movie aMovie = new Movie(ANY_TITLE,Movie.NEW_RELEASE);
+		for (int daysRented = 1; daysRented <= charges.length; daysRented++) {
+			assertEquals(charges[daysRented-1], aMovie.getCharge(daysRented),0);
+		}
+	}
+	
+	@Test
+	public void testChildrensPrice() {
+		Price aPrice = new ChildrensPrice();
+		assertEquals(Movie.CHILDRENS,aPrice.getPriceCode());
+	}
 }
