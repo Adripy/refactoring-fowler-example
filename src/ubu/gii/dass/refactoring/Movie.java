f488968ec19a6670656fb3dc4d3a89118e2956ee
Adripy <adri_cordero89@hotmail.com>
2023-05-17 21:30:39 +0200

    Mover getCharge a Movie
    movemos e metodo getCharge a la clase Movie y lo modificamos para que realice su función desde esta clase

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -35,4 +35,26 @@ public class Movie {
 	public String getTitle() {
 		return _title;
 	}
+
+	double getCharge(Rental rental) {
+		double thisAmount = 0;
+		// determine amounts for each line
+		int daysRented = rental.getDaysRented();
+		switch (rental.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (daysRented > 2)
+				thisAmount += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (daysRented > 3)
+				thisAmount += (daysRented - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
 }
