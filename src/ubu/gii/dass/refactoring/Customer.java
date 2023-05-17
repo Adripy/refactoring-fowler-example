b285b50cbb52cc564cecce6b1995a3d5c4de7b49
Adripy <adri_cordero89@hotmail.com>
2023-05-17 20:46:18 +0200

    Creamos un nuevo método llamado htmlStatement
    Creamos un nuevo método llamado htmlStatement a partir del método statement dondo solo modificaremos los texto para salida html.

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -52,4 +52,27 @@ public class Customer {
 				+ " frequent renter points";
 		return result;
 	}
+
+	public String htmlStatement() {
+		double totalAmount = 0;
+		int totalFrequentRenterPoints = 0;
+		Iterator<Rental> rentals = _rentals.iterator();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		while (rentals.hasNext()) {
+			double thisAmount = 0;
+			Rental each = rentals.next();
+			thisAmount = each.getCharge();
+			
+			totalFrequentRenterPoints += each.getFrequentRenterPoints();
+			// show figures for this rental
+			result += each.getMovie().getTitle() + ": "
+					+ String.valueOf(thisAmount) + "<BR>\n";
+			totalAmount += thisAmount;
+		}
+		// add footer lines
+		result += "<P>You owe <EM>" + String.valueOf(totalAmount) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(totalFrequentRenterPoints)
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}
 }
